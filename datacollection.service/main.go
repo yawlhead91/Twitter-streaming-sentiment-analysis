d256a8a082ab7e3c774e38a85383c34e9f73d900
yawlhead91 <alexncork91@gmail.com>
2018-03-07 16:40:55 +0000

    added viper config

--- datacollection.service/main.go
+++ datacollection.service/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 func main() {
+	// Load the config file using viper the
+	// config file holds you access keys
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
 
 }
