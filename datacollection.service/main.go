90bb4f4afa442add87df8127f300ba66b8eeb321
yawlhead91 <alexncork91@gmail.com>
2018-03-08 15:01:42 +0000

    Create inital stream and proto file

--- datacollection.service/main.go
+++ datacollection.service/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 	"github.com/spf13/viper"
 )
 
@@ -15,5 +21,37 @@ func main() {
 	if err != nil {             // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %s", err))
 	}
+
+	// The twitter package provides a Client for accessing the Twitter API.
+	// Here we create a new twitter client using the credentials in the config file
+	config := oauth1.NewConfig(viper.GetString("credentials.consumerKey"),
+		viper.GetString("credentials.consumerSecret"))
+	token := oauth1.NewToken(viper.GetString("credentials.accessToken"),
+		viper.GetString("credentials.accessSecret"))
+
+	// Authentication is handled by the http.Client which is
+	// passed to twitter NewClient as Oauth1
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	params := &twitter.StreamFilterParams{
+		Track:         []string{"bitcoin"},
+		StallWarnings: twitter.Bool(true),
+	}
+	stream, err := client.Streams.Filter(params)
+
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		fmt.Println(tweet.Text)
+	}
+
+	go demux.HandleChan(stream.Messages)
+
+	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
+
+	stream.Stop()
 
 }
