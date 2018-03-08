937fdbb8e908028558ab62cb6948e52db609c12d
yawlhead91 <alexncork91@gmail.com>
2018-03-08 20:40:42 +0000

    Return stream of data

--- datacollection_service/server/server.go
+++ datacollection_service/server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"fmt"
+
+	"github.com/dghubble/go-twitter/twitter"
 	pb "github.com/yawlhead91/Twitter-streaming-sentiment-analysis/datacollection_service/twitter_route"
-	twitter "github.com/yawlhead91/Twitter-streaming-sentiment-analysis/datacollection_service/twitterapi_client"
+	t "github.com/yawlhead91/Twitter-streaming-sentiment-analysis/datacollection_service/twitterapi_client"
 )
 
 // TwitterRouteServer Implements the generated
@@ -12,10 +15,26 @@ type TwitterRouteServer struct{}
 // GetTweets creates a stream of tweets for the given params
 // to be searched for from the twitter api
 func (s *TwitterRouteServer) GetTweets(params *pb.Params, stream pb.TwitterRoute_GetTweetsServer) error {
-	ts, err := twitter.GetTweets(params)
+	ts, err := t.GetStream(params)
 	if err != nil {
 		return err
 	}
+
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		r := &pb.Tweet{
+			CreatedAt:    tweet.CreatedAt,
+			RetweetCount: int64(tweet.RetweetCount),
+			Text:         tweet.Text,
+		}
+		if err = stream.Send(r); err != nil {
+
+			ts.Stop()
+			panic(fmt.Errorf("fatal error config file: %s", err))
+		}
+	}
+
+	demux.HandleChan(ts.Messages)
 
 	return nil
 }
