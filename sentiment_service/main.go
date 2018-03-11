48b8522cbf090c52fb8dfb83e513d62fba7e9ed5
yawlhead91 <alexncork91@gmail.com>
2018-03-11 16:15:53 +0000

    fixed untracked files

--- sentiment_service/main.go
+++ sentiment_service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	dc "github.com/yawlhead91/Twitter-streaming-sentiment-analysis/sentiment_service/datacollectionclient"
@@ -20,7 +21,7 @@ func main() {
 
 	session, err := r.CreateSession(datastoreAddr)
 	if err != nil {
-
+		log.Print("Theres is")
 		// We're wrapping the error returned from our CreateSession
 		// here to add some context to the error.
 		panic(fmt.Errorf("Could not connect to datastore with host %s - %v", datastoreAddr, err))
