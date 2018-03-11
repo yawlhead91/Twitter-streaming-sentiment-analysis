2a35461c5b99532f7b15d9fe8080171982a90a3b
yawlhead91 <alexncork91@gmail.com>
2018-03-11 15:10:11 +0000

    session

--- sentiment_service/main.go
+++ sentiment_service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	dc "github.com/yawlhead91/Twitter-streaming-sentiment-analysis/sentiment_service/datacollectionclient"
@@ -23,8 +24,10 @@ func main() {
 
 		// We're wrapping the error returned from our CreateSession
 		// here to add some context to the error.
-		panic(fmt.Errorf("Could not connect to datastore with host %s - %v", host, err))
+		panic(fmt.Errorf("Could not connect to datastore with host %s - %v", datastoreAddr, err))
 	}
+
+	log.Print("Session: ")
 
 	err = dc.StreamTweets(session.Clone())
 	if err != nil { // Handle errors reading the config file
